9e4c58afc5e1d851df4d8c570623bb42fe59154b
hemanth192211685 <133622478+hemanth192211685@users.noreply.github.com>
2023-08-14 08:32:20 +0530

    Create Summing up 12 + 22 + 32 + 42..... +n.cpp

---
+++ Summing up 12 + 22 + 32 + 42..... +n.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  long int r;
+  int();
+  printf("\nEnter the range: ");
+  scanf("%ld",&r);
+  printf("\nSum of the squares of the series is: %ld",((r*(r+1))*(2*r+1))/6);
+  return 0;
+}
